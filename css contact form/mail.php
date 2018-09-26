148bf8ab33edb1de131dfbbfe2bf9eb0eb3a93f5
TomaszPieta <tomaszlukaszpieta@gmail.com>
2018-09-26 16:09:07 +0200

    commit

---
+++ css contact form/mail.php
@@ -0,0 +1,14 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$email = $_POST['phone'];
+$message = $_POST['message'];
+
+$formcontent="From: $name \n Message: $message";
+$recipient = "thompson.san@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
